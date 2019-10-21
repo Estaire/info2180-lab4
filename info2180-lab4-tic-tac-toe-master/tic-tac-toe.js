a55d172bca6cbe74b289e00aa47f1c4a6bfd20c1
Estaire <55703470+Estaire@users.noreply.github.com>
2019-10-21 18:00:13 -0500

    Updated tic-tac-toe.js

--- info2180-lab4-tic-tac-toe-master/tic-tac-toe.js
+++ info2180-lab4-tic-tac-toe-master/tic-tac-toe.js
@@ -1,13 +1,46 @@
+let bool = true;
+
 function setBoard(){
 	let btn = document.querySelector(".btn");
-	
+	for(let i = 0; i < 9; i++){
+		document.getElementById('board').getElementsByTagName('div')[i].classList.toggle('square');
+	}
 	btn.onclick = function(){
-		for(i = 0; i < 9; i++){
-			document.getElementById('board').getElementsByTagName('div')[i].classList.toggle('square');
+		for(let i = 0; i < 9; i++){
+			document.getElementById('board').getElementsByTagName('div')[i].innerHTML = '';
+			bool = true;
 		}
 	};
 }
 
+function gameStart(){
+	let board  = Array.from(Array(9).keys());
+	const squares = document.getElementById('board').getElementsByTagName('div');
+	for(let i = 0; i < squares.length; i++){
+			document.getElementById('board').getElementsByTagName('div')[i].setAttribute('id', i);
+			squares[i].addEventListener('click', select, false);
+	}
+}
+
+function select(square){
+	const player1 = 'O';
+	const player2 = 'X';
+	let el = document.getElementById(square.target.id);
+	console.log(square.target.id);
+
+	if(bool === true){
+		el.innerHTML = 'O';
+		el.classList.toggle('O');
+		bool = false;
+	}
+	else{
+		el.innerHTML = 'X';
+		el.classList.toggle('X');
+		bool = true;
+	}
+}
+
 window.onload = function(){
 	setBoard();
+	gameStart();
 };
